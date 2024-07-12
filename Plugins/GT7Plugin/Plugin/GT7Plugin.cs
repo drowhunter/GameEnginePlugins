46aeb08e89bf9a58fd909a033da9582a64d62e03
drowhunter <jones1876@gmail.com>
2024-07-12 12:52:35 -0400

    update

--- Plugins/GT7Plugin/Plugin/GT7Plugin.cs
+++ Plugins/GT7Plugin/Plugin/GT7Plugin.cs
@@ -14,161 +14,160 @@ using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
 using System.Threading;
+
 using GT7.Properties;
+
+using PDTools.SimulatorInterface;
+
 using YawGEAPI;
 
 namespace YawVRYawVR_Game_Engine.Plugin
 {
-  [Export(typeof (Game))]
-  [ExportMetadata("Name", "Gran Turismo 7")]
-  [ExportMetadata("Version", "1.0")]
-  public class GT7Plugin : Game
-  {
-    private volatile bool running = false;
-    private Thread readthread;
-    private UdpClient receivingUdpClient;
-    private IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
-    private IMainFormDispatcher dispatcher;
-    private IProfileManager controller;
-
-    public int STEAM_ID => 241560;
-
-    public bool PATCH_AVAILABLE => true;
-
-    public string AUTHOR => "YawVR";
-
-    public string PROCESS_NAME => "";
-
-    public string Description => Resources.description;
-
-    public Image Logo => (Image) Resources.logo;
-
-    public Image SmallLogo => (Image) Resources.small;
-
-    public Image Background => (Image) Resources.background;
-
-    public LedEffect DefaultLED() => new LedEffect((EFFECT_TYPE) 1, 7, new YawColor[4]
+    [Export(typeof(Game))]
+    [ExportMetadata("Name", "Gran Turismo 7")]
+    [ExportMetadata("Version", "1.0")]
+    public class GT7Plugin : Game
     {
-      new YawColor((byte) 66, (byte) 135, (byte) 245),
-      new YawColor((byte) 80, (byte) 80, (byte) 80),
-      new YawColor((byte) 128, (byte) 3, (byte) 117),
-      new YawColor((byte) 110, (byte) 201, (byte) 12)
-    }, 25f);
+        private volatile bool _running = false;
+        private Thread _readthread;
+        
+       
+        private IMainFormDispatcher _dispatcher;
+        private IProfileManager _controller;
 
-    public List<Profile_Component> DefaultProfile()
-    {
-      List<Profile_Component> profileComponentList = new List<Profile_Component>();
-      profileComponentList.Add(new Profile_Component(4, 1, 1f, 0.0f, 0.0f, false, false, -1f, 1f, true, (ObservableCollection<ProfileMath>) null, (ProfileSpikeflatter) null, 0.0f, (ProfileComponentType) 0, (ObservableCollection<ProfileCondition>) null));
-      profileComponentList.Add(new Profile_Component(7, 1, 0.5f, 0.0f, 0.0f, false, true, -1f, 0.3f, true, (ObservableCollection<ProfileMath>) null, (ProfileSpikeflatter) null, 0.0f, (ProfileComponentType) 0, (ObservableCollection<ProfileCondition>) null));
-      profileComponentList.Add(new Profile_Component(1, 2, 0.1f, 0.0f, 0.0f, false, false, -1f, 0.05f, true, (ObservableCollection<ProfileMath>) null, (ProfileSpikeflatter) null, 0.0f, (ProfileComponentType) 0, (ObservableCollection<ProfileCondition>) null));
-      profileComponentList.Add(new Profile_Component(5, 2, 1f, 0.0f, 0.0f, false, true, -1f, 1f, true, (ObservableCollection<ProfileMath>) null, (ProfileSpikeflatter) null, 0.0f, (ProfileComponentType) 0, (ObservableCollection<ProfileCondition>) null));
-      profileComponentList.Add(new Profile_Component(0, 1, 0.1f, 0.0f, 0.0f, false, false, -1f, 1f, true, (ObservableCollection<ProfileMath>) null, (ProfileSpikeflatter) null, 0.0f, (ProfileComponentType) 0, (ObservableCollection<ProfileCondition>) null));
-      profileComponentList.Add(new Profile_Component(3, 0, 1f, 0.0f, 0.0f, false, true, -1f, 1f, true, (ObservableCollection<ProfileMath>) null, (ProfileSpikeflatter) null, 0.0f, (ProfileComponentType) 0, (ObservableCollection<ProfileCondition>) null));
-      return profileComponentList;
-    }
+        private CancellationTokenSource _cts;
+        private SimulatorInterfaceClient _simInterface;
 
-    public void Exit()
-    {
-      this.receivingUdpClient.Close();
-      this.receivingUdpClient = (UdpClient) null;
-      this.running = false;
-    }
 
-    public string[] GetInputData() => new string[15]
-    {
-      "AngularVelocityP",
-      "AngularVelocityR",
-      "AngularVelocityY",
-      "OrientationYaw",
-      "OrientationPitch",
-      "OrientationRoll",
-      "Acceleration_Lateral",
-      "Acceleration_longitudinal",
-      "Acceleration_vertical",
-      "VelocityX",
-      "VelocityY",
-      "VelocityZ",
-      "PositionX",
-      "PositionY",
-      "PositionZ"
-    };
+        private const float _toDegree = 57.295f;
+        public int STEAM_ID => 241560;
 
-    public void SetReferences(IProfileManager controller, IMainFormDispatcher dispatcher)
-    {
-      this.dispatcher = dispatcher;
-      this.controller = controller;
-    }
+        public bool PATCH_AVAILABLE => true;
 
-    public void Init()
-    {
-      this.running = true;
-      this.receivingUdpClient = new UdpClient(1337);
-      this.readthread = new Thread(new ThreadStart(this.ReadFunction));
-      this.readthread.Start();
-    }
+        public string AUTHOR => "Drowhunter";
 
-    
+        public string PROCESS_NAME => "";
 
-    private void ReadFunction()
-    {
-      while (this.running)
-      {
-        byte[] numArray = this.receivingUdpClient.Receive(ref this.RemoteIpEndPoint);
-        float single1 = BitConverter.ToSingle(numArray, 4);
-        float single2 = BitConverter.ToSingle(numArray, 8);
-        float single3 = BitConverter.ToSingle(numArray, 12);
-        float single4 = BitConverter.ToSingle(numArray, 16);
-        float single5 = BitConverter.ToSingle(numArray, 20);
-        float single6 = BitConverter.ToSingle(numArray, 24);
-        float single7 = BitConverter.ToSingle(numArray, 28);
-        float single8 = BitConverter.ToSingle(numArray, 32);
-        float single9 = BitConverter.ToSingle(numArray, 36);
-        float single10 = BitConverter.ToSingle(numArray, 40);
-        float single11 = BitConverter.ToSingle(numArray, 44);
-        float single12 = BitConverter.ToSingle(numArray, 48);
-        float single13 = BitConverter.ToSingle(numArray, 52);
-        float single14 = BitConverter.ToSingle(numArray, 56);
-        float single15 = BitConverter.ToSingle(numArray, 60);
-        BitConverter.ToSingle(numArray, 64);
-        this.controller.SetInput(0, single1 * 57.295f);
-        this.controller.SetInput(1, single2 * 57.295f);
-        this.controller.SetInput(2, single3 * 57.295f);
-        this.controller.SetInput(3, single4 * 57.295f);
-        this.controller.SetInput(4, single5 * 57.295f);
-        this.controller.SetInput(5, single6 * 57.295f);
-        this.controller.SetInput(6, single7);
-        this.controller.SetInput(7, single8);
-        this.controller.SetInput(8, single9);
-        this.controller.SetInput(9, single10);
-        this.controller.SetInput(10, single11);
-        this.controller.SetInput(11, single12);
-        this.controller.SetInput(12, single13);
-        this.controller.SetInput(13, single14);
-        this.controller.SetInput(14, single15);
-      }
-    }
+        public string Description => Resources.description;
 
-    public void PatchGame()
-    {
-      try
-      {
-        string str = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "/The Crew";
-        Console.WriteLine(str);
-        string tempFileName = Path.GetTempFileName();
-        this.dispatcher.GetInstallPath(str);
-        using (WebClient webClient = new WebClient())
+        public Image Logo => (Image)Resources.logo;
+
+        public Image SmallLogo => (Image)Resources.small;
+
+        public Image Background => (Image)Resources.background;
+
+        public LedEffect DefaultLED() => new LedEffect((EFFECT_TYPE)1, 7, new YawColor[4]
         {
-          webClient.DownloadFile("http://yaw.one/gameengine/Plugins/The_Crew_2/GT72.zip", tempFileName);
-          Console.WriteLine(str);
-          this.dispatcher.ExtractToDirectory(tempFileName, str, true);
-        }
-      }
-      catch (Exception ex)
-      {
-        Console.WriteLine("{0}", (object) ex.ToString());
-      }
-    }
+              new YawColor((byte) 66, (byte) 135, (byte) 245),
+              new YawColor((byte) 80, (byte) 80, (byte) 80),
+              new YawColor((byte) 128, (byte) 3, (byte) 117),
+              new YawColor((byte) 110, (byte) 201, (byte) 12)
+        }, 25f);
 
-    public Dictionary<string, ParameterInfo[]> GetFeatures() => (Dictionary<string, ParameterInfo[]>) null;
-  }
+        public List<Profile_Component> DefaultProfile()
+        {
+            List<Profile_Component> profileComponentList = new List<Profile_Component>();
+            profileComponentList.Add(new Profile_Component(4, 1, 1f, 0.0f, 0.0f, false, false, -1f, 1f, true, (ObservableCollection<ProfileMath>)null, (ProfileSpikeflatter)null, 0.0f, (ProfileComponentType)0, (ObservableCollection<ProfileCondition>)null));
+            profileComponentList.Add(new Profile_Component(7, 1, 0.5f, 0.0f, 0.0f, false, true, -1f, 0.3f, true, (ObservableCollection<ProfileMath>)null, (ProfileSpikeflatter)null, 0.0f, (ProfileComponentType)0, (ObservableCollection<ProfileCondition>)null));
+            profileComponentList.Add(new Profile_Component(1, 2, 0.1f, 0.0f, 0.0f, false, false, -1f, 0.05f, true, (ObservableCollection<ProfileMath>)null, (ProfileSpikeflatter)null, 0.0f, (ProfileComponentType)0, (ObservableCollection<ProfileCondition>)null));
+            profileComponentList.Add(new Profile_Component(5, 2, 1f, 0.0f, 0.0f, false, true, -1f, 1f, true, (ObservableCollection<ProfileMath>)null, (ProfileSpikeflatter)null, 0.0f, (ProfileComponentType)0, (ObservableCollection<ProfileCondition>)null));
+            profileComponentList.Add(new Profile_Component(0, 1, 0.1f, 0.0f, 0.0f, false, false, -1f, 1f, true, (ObservableCollection<ProfileMath>)null, (ProfileSpikeflatter)null, 0.0f, (ProfileComponentType)0, (ObservableCollection<ProfileCondition>)null));
+            profileComponentList.Add(new Profile_Component(3, 0, 1f, 0.0f, 0.0f, false, true, -1f, 1f, true, (ObservableCollection<ProfileMath>)null, (ProfileSpikeflatter)null, 0.0f, (ProfileComponentType)0, (ObservableCollection<ProfileCondition>)null));
+            return profileComponentList;
+        }
+
+        public void Exit()
+        {
+            _cts.Cancel();
+            _simInterface.Dispose();
+            
+            this._running = false;
+        }
+
+        
+
+        public string[] GetInputData() => new string[12]
+        {
+          "AngularVelocityP", "AngularVelocityR", "AngularVelocityY",
+          "OrientationYaw", "OrientationPitch", "OrientationRoll",
+          //"Acceleration_Lateral", "Acceleration_longitudinal", "Acceleration_vertical",
+          "VelocityX", "VelocityY", "VelocityZ",
+          "PositionX", "PositionY", "PositionZ"
+        };
+
+        public void SetReferences(IProfileManager controller, IMainFormDispatcher dispatcher)
+        {
+            this._dispatcher = dispatcher;
+            this._controller = controller;
+        }
+
+        public void Init()
+        {
+            this._running = true;
+
+            
+            _simInterface = new SimulatorInterfaceClient("192.168.50.226", SimulatorInterfaceGameType.GT7);
+            _simInterface.OnReceive += SimInterface_OnReceive;
+
+
+            _cts = new CancellationTokenSource();
+
+            // Cancel token from outside source to end simulator
+
+            
+
+            
+            
+            
+            //this._readthread = new Thread(new ThreadStart(this.ReadFunction));
+            //this._readthread.Start();
+        }
+        
+        private void SimInterface_OnReceive(SimulatorPacket packet)
+        {
+            // Print the packet contents to the console
+            Console.SetCursorPosition(0, 0);
+            packet.PrintPacket(_running);
+
+            // Get the game type the packet was issued from
+            SimulatorInterfaceGameType gameType = packet.GameType;
+
+            // Check on flags for whether the simulation is active
+            if (packet.Flags.HasFlag(SimulatorFlags.CarOnTrack) && !packet.Flags.HasFlag(SimulatorFlags.Paused) && !packet.Flags.HasFlag(SimulatorFlags.LoadingOrProcessing))
+            {
+                // Do stuff with packet
+                
+
+                _controller.SetInput(0, packet.AngularVelocity.X * _toDegree);
+                _controller.SetInput(1, packet.AngularVelocity.Y * _toDegree);
+                _controller.SetInput(2, packet.AngularVelocity.Z * _toDegree);
+
+                _controller.SetInput(3, packet.Rotation.X * _toDegree);
+                _controller.SetInput(4, packet.Rotation.Y * _toDegree);
+                _controller.SetInput(5, packet.Rotation.Z * _toDegree);
+
+                //_controller.SetInput(6, packet..X);
+                //_controller.SetInput(7, packet.Velocity.Y);
+                //_controller.SetInput(8, packet.Velocity.Z);
+
+                _controller.SetInput(6, packet.Velocity.X);
+                _controller.SetInput(7, packet.Velocity.Y);
+                _controller.SetInput(8, packet.Velocity.Z);
+
+                _controller.SetInput(09, packet.Position.X);
+                _controller.SetInput(10, packet.Position.Y);
+                _controller.SetInput(11, packet.Position.Z);
+
+            }
+        }
+
+        
+
+        public void PatchGame()
+        {
+            
+        }
+
+        public Dictionary<string, ParameterInfo[]> GetFeatures() => (Dictionary<string, ParameterInfo[]>)null;
+    }
 }
