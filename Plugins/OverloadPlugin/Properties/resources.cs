025f32374a8a1bf53db763b1fbb0d661c4a98e09
phunkaeg <36734585+phunkaeg@users.noreply.github.com>
2024-07-26 15:57:04 +1000

    Create resources.cs

---
+++ Plugins/OverloadPlugin/Properties/resources.cs
@@ -0,0 +1,73 @@
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+
+namespace Overload.Properties
+{
+  [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+  [DebuggerNonUserCode]
+  [CompilerGenerated]
+  public class Resources
+  {
+    private static ResourceManager resourceMan;
+    private static CultureInfo resourceCulture;
+
+    internal Resources()
+    {
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static ResourceManager ResourceManager
+    {
+      get
+      {
+        if (OverloadPlugin.Properties.Resources.resourceMan == null)
+          OverloadPlugin.Properties.Resources.resourceMan = new ResourceManager("OverloadPlugin.Properties.Resources", typeof (OverloadPlugin.Properties.Resources).Assembly);
+        return OverloadPlugin.Properties.Resources.resourceMan;
+      }
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    public static CultureInfo Culture
+    {
+      get => OverloadPlugin.Properties.Resources.resourceCulture;
+      set => OverloadPlugin.Properties.Resources.resourceCulture = value;
+    }
+
+    public static Bitmap background
+    {
+      get
+      {
+        return (Bitmap) OverloadPlugin.Properties.Resources.ResourceManager.GetObject(nameof (background), OverloadPlugin.Properties.Resources.resourceCulture);
+      }
+    }
+
+    public static string defProfile
+    {
+      get => OverloadPlugin.Properties.Resources.ResourceManager.GetString(nameof (defProfile), OverloadPlugin.Properties.Resources.resourceCulture);
+    }
+
+    public static string description
+    {
+      get => OverloadPlugin.Properties.Resources.ResourceManager.GetString(nameof (description), OverloadPlugin.Properties.Resources.resourceCulture);
+    }
+
+    public static Bitmap logo
+    {
+      get => (Bitmap) OverloadPlugin.Properties.Resources.ResourceManager.GetObject(nameof (logo), OverloadPlugin.Properties.Resources.resourceCulture);
+    }
+
+    public static Bitmap small
+    {
+      get
+      {
+        return (Bitmap) OverloadPlugin.Properties.Resources.ResourceManager.GetObject(nameof (small), OverloadPlugin.Properties.Resources.resourceCulture);
+      }
+    }
+  }
+}
