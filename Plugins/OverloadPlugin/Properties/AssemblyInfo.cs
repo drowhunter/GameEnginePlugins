5500b5dff5544eae184a1a9d3aa0b0e24b6cfe8a
phunkaeg <36734585+phunkaeg@users.noreply.github.com>
2024-07-26 15:40:17 +1000

    Create AssemblyInfo.cs

---
+++ Plugins/OverloadPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OverloadPlugin")]
+[assembly: AssemblyDescription("Overload Plugin for YAW")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OverloadPlugin")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("A3CC01F7-9AA4-4E93-8F79-5A7B10E98ABE")]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+
+[assembly: InternalsVisibleTo("GT7PluginTest")]
